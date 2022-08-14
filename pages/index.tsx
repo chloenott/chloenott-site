b21c45febf838cf99680570dec73ca228952dbaa
Chloe Nott <89756479+chloenott@users.noreply.github.com>
2022-08-14 07:27:39 -0700

    Add: external logos; section text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -20,6 +21,18 @@ const Home: NextPage = () => {
           <h2 className={styles.subtitle}>
             Personal Website
           </h2>
+        </div>
+        <div className={styles.section_pagewidth}>
+          <section className={styles.section_block}>
+            <h2 className={styles.section_title}>Welcome</h2>
+            <p className={styles.section_paragraph}>I really like Gundam Unicorn...</p>
+          </section>
+          <section className={styles.section_block}>
+            <div className={styles.logo_block}>
+            <a href="https://www.linkedin.com/in/chloenott/"><img src="/icons/iconmonstr-linkedin-2.svg" alt="LinkedIn logo"></img></a>
+            <a href="https://github.com/chloenott"><img src="/icons/iconmonstr-github-2.svg" alt="GitHub logo"></img></a>
+            </div>
+          </section>
         </div>
       </main>
     </div>
