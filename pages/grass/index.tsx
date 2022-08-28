8233373362cedbac55db1e4ad417c28007ebcffc
Chloe Nott <89756479+chloenott@users.noreply.github.com>
2022-08-28 08:43:12 -0700

    Attempt to get vercel deployment to not crash on messageList being empty. Turns out empty arrays in javascript are truthy

--- pages/grass/index.tsx
+++ pages/grass/index.tsx
@@ -104,7 +104,7 @@ const AnotherWorld: NextPage = () => {
         <div className={chatVisible ? styles.chat_window_visible : styles.chat_window_hidden}>
 
           <div className={styles.message_list}>
-            {messageList && messageList.map( (message, index) => <MessageContainer key={index} message={message}/>)}
+            {messageList.length > 0 && messageList.map( (message, index) => <MessageContainer key={index} message={message}/>)}
           </div>
 
           <Divider variant="middle" textAlign="center" sx={{
