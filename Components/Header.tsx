56fe6276953ad8929af5c976b00183e5e449d9f7
Chloe Nott <89756479+chloenott@users.noreply.github.com>
2022-09-01 17:11:15 -0700

    Revert "Placeholder version santized of personal information (visible only)"
    This reverts commit fae35e6bd66b0dffb60cc32670826e62fc7895b9.

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <>
       <Head>
-        <title>[PLACEHOLDER]</title>
+        <title>chloenott.xyz</title>
         <meta name="chloe nott" content="so we meet again..." />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -15,8 +15,7 @@ const Header = () => {
       </Head>
 
       <div className={styles.title_container}>
-        {/* <Link href="/" className={styles.homeLink}><img width="75" height="75" src="/icons/titleicon.svg"></img></Link> */}
-        <Link href="/" className={styles.homeLink}><img width="75" height="75" src="/icons/iconmonstr-github-2.svg"></img></Link>
+        <Link href="/" className={styles.homeLink}><img width="75" height="75" src="/titleicon.svg"></img></Link>
         <div className={styles.logo_block}>
           <a href="https://github.com/chloenott"><img width="35" height="35" src="/icons/iconmonstr-github-2.svg" alt="GitHub logo"></img></a>
           <a href="https://www.linkedin.com/in/chloenott/"><img width="35" height="35" src="/icons/iconmonstr-linkedin-2.svg" alt="LinkedIn logo"></img></a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,7 @@ const Home: NextPage = () => {
         <section className={styles.chart_info_card} id="chart_info_card">
           <p className={styles.card_title}>Living Reduction</p><p className={styles.card_year}>, 2022</p>
           <p className={styles.card_medium}>D3 in Next.js</p>
-          {/* <p className={styles.card_description}>{`Chloe reduced to keywords. Their data visualization experience comes from a career in sensor R&D and manufacture where Tableau, Python, Minitab, Excel, and SQL were used to realize product insights and motivate changes. But wait, there's more!`}</p> */}
-          <p className={styles.card_description}>{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`}</p>
+          <p className={styles.card_description}>{`Chloe reduced to keywords. Their data visualization experience comes from a career in sensor R&D and manufacture where Tableau, Python, Minitab, Excel, and SQL were used to realize product insights and motivate changes. But wait, there's more!`}</p>
           <p className={styles.card_instructions}>Instructions: Find the hidden link to proceed.</p>
         </section>
       </main>
