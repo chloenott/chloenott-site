2b4b888ed47ac71c378e4ca74397a8b9f4c89541
Chloe Nott <89756479+chloenott@users.noreply.github.com>
2022-11-16 12:59:33 -0800

    Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import '../public/fonts/ovo.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../Components/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
